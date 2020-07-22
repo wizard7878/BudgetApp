baa178f79888501d090ad41f96a442ef0409b735
Wizard7878 <peyman934@gmail.com>
2020-07-22 13:13:05 +0430

    get_inputs

--- app.js
+++ app.js
@@ -9,14 +9,31 @@ function BudgetController(){
 function UIController(){
 
     var DomStrings = {
-        btn_submit : '.add__btn'
+        btn_submit : '.add__btn',
+        type: '.add__type',
+        description : '.add__description',
+        value : '.add__value'
     }
 
 
 
 
     return{
-        Dom_Selectors : DomStrings
+        Dom_Selectors : DomStrings,
+        get_inputs : function(){
+            var type,description,value
+
+            type = document.querySelector(DomStrings.type).value
+            description = document.querySelector(DomStrings.description).value
+            value = document.querySelector(DomStrings.value).value
+
+
+            return {
+                type:type,
+                description:description,
+                value:value
+            }
+        }
     }
 }
 
@@ -30,17 +47,26 @@ var Controller = (function (budget_ctrl,ui_ctrl){
     function setupEventListener(){
         document.querySelector(Dom.btn_submit).addEventListener('click',()=>{
             console.log("Clicked")
+            input()
         })
         document.addEventListener('keypress',(event) =>{
             if(event.which === 13 || event.keyCode === 13){
                 console.log("Entered!")
+                input()
             }
         })
+    }
+
+
+    var input = function(){
+        var get_inputs = ui_ctrl.get_inputs()
+        console.log(get_inputs)
     }
 
     return {
         init:function(){
             setupEventListener()
+            
         }
     }
 
